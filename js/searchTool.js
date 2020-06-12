da93ccbd41cb3b67d9e033816fa5eb9750567d0a
Yayo <yceballost@gmail.com>
2020-06-12 19:03:10 +0200

    new design and searchBar

---
+++ js/searchTool.js
@@ -0,0 +1,45 @@
+var busqueda = "";
+var icnSelected = [];
+
+// input de datos cuando tecleas
+$("#searchInput").keyup(function () {
+    icnSelected = [];
+  $(".card").show();
+  // guarda los datos en la variable
+  let searchValue = $("#searchInput").val().toLowerCase();
+  if (searchValue == ""){
+    $(".card").show();
+    return;
+  }
+
+  for (var icn in icnTags){
+    if (icn.startsWith(searchValue) && !icnSelected.includes(icn)){
+      icnSelected.push(icn)
+    }
+    var icnTag = icnTags[icn];
+    $.each(icnTag, function(key, tag){
+      if (tag.startsWith(searchValue) && !icnSelected.includes(icn)){
+        icnSelected.push(icn)
+      }
+    });
+  }
+
+  $(".card").hide();
+  $("#icn_" + searchValue).show();
+
+  $.each(icnSelected, function (arrayPosition, icnValue){
+    $("#icn_" + icnValue).show();
+  });
+  $(".card").each(function (arrayPosition){
+    var iconName = this.id.substring(4);
+    var iconNameSplitted = iconName.split("_");
+    // console.log(iconNameSplitted);
+
+    if(iconName.includes(searchValue)){
+      $(this).show();
+      }
+  });
+});
+
+
+$("#searchInput").focus();
